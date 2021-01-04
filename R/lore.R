0aac88d03020971884f04ae03505046c98ded4f1
luukvdmeer <luukvandermeer@live.nl>
2021-01-04 15:37:25 +0100

    Correct file paths :bear:

--- R/lore.R
+++ R/lore.R
@@ -1,7 +1,7 @@
 #' Lore is saying hi!
 #' @export
 lore_says_hi = function() {
-  g = magick::image_read("img/hi.gif")
+  g = magick::image_read(system.file("img/hi.gif"))
   print(g, info = FALSE)
   message("Hi Lore! ", emo::ji("wave"),  " Como va? I am ", emo::ji("bear"), "! I am here to cheer you up whenever needed! ", emo::ji("tada"))
 }
@@ -9,7 +9,7 @@ lore_says_hi = function() {
 #' Lore is saying bye!
 #' @export
 lore_says_bye = function() {
-  g = magick::image_read("img/bye.gif")
+  g = magick::image_read(system.file("img/bye.gif"))
   print(g, info = FALSE)
   message("Bye Lore! ", emo::ji("wave"),  " Hope you had a wonderful day! Tell ", emo::ji("bear"), " everything about it when you get home! Be safe!")
 }
@@ -17,7 +17,7 @@ lore_says_bye = function() {
 #' Lore is bored...
 #' @export
 lore_is_bored = function() {
-  g = magick::image_read("img/bored.gif")
+  g = magick::image_read(system.file("img/bored.gif"))
   print(g, info = FALSE)
   message("No worries Lore! ", emo::ji("bear"), " will tell you a funny story! One day ", emo::ji("bear"), " was walking in the woods to look for berries ", emo::ji("strawberry"), " Then he saw two bunnies ", emo::ji("rabbit"), emo::ji("rabbit"), " hopping! ", emo::ji("bear"), " grunted: 'Hello there, who are you two? I have never seen you hopping here before! Are you on holiday?' The bunnies answered: 'Yes! We are just married! We are on our bunnymoon!' ", emo::ji("rofl"))
 }
@@ -25,7 +25,7 @@ lore_is_bored = function() {
 #' Lore is excited!!
 #' @export
 lore_is_excited = function() {
-  g = magick::image_read("img/excited.gif")
+  g = magick::image_read(system.file("img/excited.gif"))
   print(g, info = FALSE)
   message(emo::ji("bear"), " is also excited!! Let's do this!! Mana Mana Tutu Tururu ", emo::ji("notes"))
 }
@@ -33,7 +33,7 @@ lore_is_excited = function() {
 #' Lore is frustrated...
 #' @export
 lore_is_frustrated = function() {
-  g = magick::image_read("img/frustrated.gif")
+  g = magick::image_read(system.file("img/frustrated.gif"))
   print(g, info = FALSE)
   message("That is ok Lore! Just let it all out! Take a small break from the things you are doing. You R super smart and skillful and you will make it work! ", emo::ji("bear"), " has a lot of faith in you! ", emo::ji("woman"), emo::ji("muscle"))
 }
@@ -41,7 +41,7 @@ lore_is_frustrated = function() {
 #' Lore is happy!!
 #' @export
 lore_is_happy = function() {
-  g = magick::image_read("img/happy.gif")
+  g = magick::image_read(system.file("img/happy.gif"))
   print(g, info = FALSE)
   message("Yipieeeyeee!! Do the happy dance!! ", emo::ji("dancer"), emo::ji("rabbit"), emo::ji("bear"))
 }
@@ -49,7 +49,7 @@ lore_is_happy = function() {
 #' Lore is lonely...
 #' @export
 lore_is_lonely = function() {
-  g = magick::image_read("img/lonely.gif")
+  g = magick::image_read(system.file("img/lonely.gif"))
   print(g, info = FALSE)
   message("Think about ", emo::ji("bear"), " and know that ", emo::ji("bear"), " is also thinking of you!")
 }
@@ -57,7 +57,7 @@ lore_is_lonely = function() {
 #' Lore is sad...
 #' @export
 lore_is_sad = function() {
-  g = magick::image_read("img/sad.gif")
+  g = magick::image_read(system.file("img/sad.gif"))
   print(g, info = FALSE)
   message("Lore, just remember: It is most important that you are loved, and ", emo::ji("bear"), " loves you a lot! ", emo::ji("rabbit"), emo::ji("heart"), emo::ji("bear"), " And he is not the only one! Your family, your friends. They may be far away but they carry you in their hearts forever! ", emo::ji("heart"))
 }
@@ -65,7 +65,7 @@ lore_is_sad = function() {
 #' Lore is tired...
 #' @export
 lore_is_tired = function() {
-  i = magick::image_read("img/active.png")
+  i = magick::image_read(system.file("img/active.png"))
   print(i, info = FALSE)
   message("Active break time!! ", emo::ji("dancer"), " Stand up and follow the guidelines in the plot!")
 }
@@ -73,7 +73,7 @@ lore_is_tired = function() {
 #' Lore is feeling stupid...
 #' @export
 lore_is_feeling_stupid = function() {
-  g = magick::image_read("img/stupid.gif")
+  g = magick::image_read(system.file("img/stupid.gif"))
   print(g, info = FALSE)
   message("Take that back Lore! You R awesome and ", emo::ji("bear"), " is super super proud of you!")
 }
@@ -81,7 +81,7 @@ lore_is_feeling_stupid = function() {
 #' Lore is feeling ugly...
 #' @export
 lore_is_feeling_ugly = function() {
-  g = magick::image_read("img/ugly.gif")
+  g = magick::image_read(system.file("img/ugly.gif"))
   print(g, info = FALSE)
   message("You are a bit crazy Lore! You are the most wonderful, beautiful being ", emo::ji("bear"), " has ever seen!")
 }
@@ -89,7 +89,7 @@ lore_is_feeling_ugly = function() {
 #' Lore is feeling useless...
 #' @export
 lore_is_feeling_useless = function() {
-  g = magick::image_read("img/useless.gif")
+  g = magick::image_read(system.file("img/useless.gif"))
   print(g, info = FALSE)
   message("Everybody sometimes feels this way Lore. But you are wrong! Just think of the road you took to get here. All the things you did, all the battles you fought. You R a power woman and ", emo::ji("bear"), " is super super proud of you! ", emo::ji("woman"), emo::ji("muscle"))
 }
@@ -97,7 +97,7 @@ lore_is_feeling_useless = function() {
 #' Lore is needing attention!!
 #' @export
 lore_is_needing_attention = function() {
-  g = magick::image_read("img/attention.gif")
+  g = magick::image_read(system.file("img/attention.gif"))
   print(g, info = FALSE)
   message("OK! ", emo::ji("bear"), " just stopped his work and thinks only of you Lore!")
 }
